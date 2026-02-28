b7a5309f93b87a4762efe6b848e527de8443339b
valdez <leonardovaldez.sn@gmail.com>
2026-02-28 05:13:44 -0300

    Add CreditCard, Purchase and Main classes

---
+++ src/CreditCard.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class CreditCard {
+    private double limit;
+    private double balance;
+    private List<Purchase> purchases;
+
+    public CreditCard(double limit) {
+        this.limit = limit;
+        this.balance = limit;
+        this.purchases = new ArrayList<>();
+    }
+
+    public boolean processPurchase(Purchase purchase) {
+        if (this.balance > purchase.getPrice()) {
+            this.balance -= purchase.getPrice();
+            this.purchases.add(purchase);
+            return true;
+        }
+        return false;
+    }
+
+    public double getLimit() {
+        return limit;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public List<Purchase> getPurchases() {
+        return purchases;
+    }
+}
