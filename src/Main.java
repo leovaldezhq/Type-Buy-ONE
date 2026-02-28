7109e8fad77a09b709530013b4b7715785d40237
valdez <leonardovaldez.sn@gmail.com>
2026-02-28 05:35:30 -0300

    Add Main.java with menu for purchases and purchase history exhibition

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,38 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
+        System.out.println("Enter your credit card limit:");
+        double limit  = input.nextDouble();
+        CreditCard card = new CreditCard(limit);
 
+        int quit = 1;
+        while (quit != 0) {
+            System.out.println("What are you buying?");
+            String description = input.next();
+
+            System.out.println("How much does your item cost?");
+            double price = input.nextDouble();
+
+            Purchase purchase1 = new Purchase(description, price);
+            boolean completePurchase = card.processPurchase(purchase1);
+
+            if (completePurchase) {
+                System.out.println("Purchase successful!");
+                System.out.println("Type 0 to quit or 1 to keep shopping!");
+                quit = input.nextInt();
+            } else {
+                System.out.println("Purchase unsuccessful!");
+                quit = 0;
+            }
+        }
+
+        System.out.println("-----------------");
+        System.out.println("Purchase history: ");
+        for (Purchase p : card.getPurchases())  {
+            System.out.println(p.getDescription() + " $ " + p.getPrice());
+        }
+        System.out.println("-----------------");
+
+        System.out.println("Remaining balance: $ " + card.getBalance());
     }
 }
